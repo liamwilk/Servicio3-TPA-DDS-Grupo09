10770702f3b969ccadc75494ea76ccabdb2b6995
AAMazzini03 <amazzini@frba.utn.edu.ar>
2024-09-08 18:32:32 -0300

    Saco TODO ya no es un trabajo por hacer para nosotros, sino que es una indicacion de donde deberia realizar la logica con los datos obtenidos quien se conecta a nuestra API

--- src/main/java/rest/consumer/VulnerablesPorBarriosCronJob.java
+++ src/main/java/rest/consumer/VulnerablesPorBarriosCronJob.java
@@ -20,7 +20,9 @@ public class VulnerablesPorBarriosCronJob {
   public static void main(String[] args) {
     RestTemplate restTemplate = new RestTemplate();
     String jsonResponse = restTemplate.getForObject("http://localhost:8080/api/atencion-medica/localidadesVulnerables", String.class);
-    //TODO: agregar logica correspondiente
+
+    // Aqui es donde se deberia realizar la logica con los datos obtenidos.
+
     log.info(jsonResponse);
   }
 }
